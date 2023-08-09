33f8644e93bfd0241698aa8dc826f1a608a2f410
renawmontanari <regiangelica@gmail.com>
2023-08-08 23:49:59 -0300

    React-Router

--- reactrouter/src/components/Navbar.js
+++ reactrouter/src/components/Navbar.js
@@ -1,13 +1,15 @@
 // Links com react router
 import "./Navbar.css";
 
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <nav>
-        <Link to="/">Home</Link>
-        <Link to="/about">Sobre</Link>
+        {/*<Link to="/">Home</Link>
+        <Link to="/about">Sobre</Link>*/}
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/about">Sobre</NavLink>
     </nav>
   )
 }
